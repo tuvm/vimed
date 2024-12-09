dea06eea0056f6a128098c333b4b78644f14a5b4
Vu Minh Tu <tu.vm41@gmail.com>
2024-12-09 15:46:33 +0700

    Check update and notify

--- projects/viewer-desktop/main/index.ts
+++ projects/viewer-desktop/main/index.ts
@@ -1,6 +1,7 @@
 import { BrowserWindow, app } from 'electron';
 import { resolve } from 'path';
 import isDev from 'electron-is-dev';
+import { autoUpdater } from 'electron-updater';
 
 if (app.requestSingleInstanceLock()) {
   app.whenReady().then(() => {
@@ -25,6 +26,18 @@ if (app.requestSingleInstanceLock()) {
     } else {
       window.loadFile('.vite/renderer/index.html');
     }
+
+    autoUpdater.checkForUpdatesAndNotify();
+
+  autoUpdater.on('update-available', () => {
+    console.log('Update available');
+    window.webContents.send('update_available');
+  });
+
+  autoUpdater.on('update-downloaded', () => {
+    console.log('Update downloaded');
+    window.webContents.send('update_downloaded');
+  });
 
     window.show();
   });
--- projects/viewer-desktop/renderer/src/App.tsx
+++ projects/viewer-desktop/renderer/src/App.tsx
@@ -1,6 +1,10 @@
 import { version } from '../../package.json';
 
 function App() {
+  window.api.onUpdateAvailable((newVersion) => {
+    alert('A new update is available. Downloading now...');
+});
+
   return (
     <div><div>Hello Desktop</div><div>Version: 1.0.0</div></div>);
 }
